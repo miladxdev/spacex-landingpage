ab3d22dcb99044989a4d2284ab07fad559a59629
Milad <miladxgh@outlook.com>
2021-07-05 14:59:36 +0430

    responsive navbar

---
+++ script.js
@@ -0,0 +1,21 @@
+// Hide Menu on scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0"; // show
+  } else {
+    document.getElementById("navbar").style.top = "-80px"; // hide
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+//toggle menu
+function toggleMenu() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " navbar-phone";
+  } else {
+    x.className = "navbar";
+  }
+}
